44ee0c3bea9e966015ded7c4dbe67c15242665e3
GoldPainel <118371288+GoldPainel@users.noreply.github.com>
2022-11-16 12:30:54 -0300

    PAINEL_BY_GOLDZIIN

---
+++ install.sh
@@ -0,0 +1,14 @@
+green='\033[0;32m' 
+apt update
+apt upgrade 
+pip install python 
+pip install python2 
+pip install python3 
+pip install os 
+pip install termcolor 
+pip install requests 
+pip install stdiomask
+
+echo "clear"
+echo "${green} [*] Todas as dependencias foram instaladas, INICIANDO SCRIPT... caso nao inicie digite \"sh start.sh\" para iniciar o script"
+sh start.sh
