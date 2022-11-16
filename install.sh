c67339e9060fa0f701588320071a45abdc831786
GoldPainel <118371288+GoldPainel@users.noreply.github.com>
2022-11-16 12:35:28 -0300

    INSTALADOR DAS DEPENDENCIAS

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 green='\033[0;32m' 
-apt update
-apt upgrade 
-pip install python 
-pip install python2 
-pip install python3 
-pip install os 
-pip install termcolor 
-pip install requests 
-pip install stdiomask
+apt update -y
+apt upgrade -y
+pip install python -y
+pip install python2 -y
+pip install python3 -y
+pip install os -y
+pip install termcolor -y 
+pip install requests -y
+pip install stdiomask -y
 
 echo "clear"
 echo "${green} [*] Todas as dependencias foram instaladas, INICIANDO SCRIPT... caso nao inicie digite \"sh start.sh\" para iniciar o script"
